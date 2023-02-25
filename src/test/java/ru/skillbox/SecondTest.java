f017dbb26c3837a027988cbfd0dfc861f6519ed4
Тменов Дмитрий <dmtconst11@gmail.com>
2023-02-25 18:22:11 +0300

    add allure test

--- src/test/java/ru/skillbox/SecondTest.java
+++ src/test/java/ru/skillbox/SecondTest.java
@@ -4,11 +4,22 @@ import dev.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SecondTest {
     public String exited = "111";
 
     @Test
     public void check(){
         Assertions.assertEquals(exited,"111");
+    }
+
+    @Test
+    void amazon(){
+        open("https://amazon.com");
+        $("#twotabsearchtextbox").setValue("Harry Potter").pressEnter();
+        $$("[data-component-type=s-search-result]").first()
+                .shouldHave(text("Harry Potter"));
     }
 }
